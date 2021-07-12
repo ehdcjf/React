41d80ed63dc48c3bcd22029b5aa0acc8be4e21ec
ehdcjf <kdch0823@naver.com>
2021-07-12 15:47:00 +0900

    memo

---
+++ webpack0712/Store/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const User = { 
+  userid:'Moon', 
+  name:'hwohwohwo'
+}
+
+const UserContext = React.createContext('kkk')//디폴드값 선언가능 객체도 가능 
+
+export default UserContext
+
+
+// 저장공간 만들기 
+//저장위치 선정하기
+// 저장한 값 가져오기
---
+++ webpack0712/component/Button.jsx
@@ -0,0 +1,24 @@
+  
+import React,{Component} from 'react'
+import UserContext from '../Store/User'
+
+class Button extends React.Component{
+    name = "방가방가"
+    static contextType = UserContext
+    render(){
+      console.log(this.context);
+        return (
+            <>
+              <ul>
+                <li>{this.context.userid}</li>
+                <li>{this.context.signaturesound}</li>
+                <li>{this.context.userjob}</li>
+              </ul>
+              <button onClick={()=>this.props.onClick(this.name)}>로그인</button>
+              <button onClick={()=>alert('userid')}>로그인2</button>
+            </>
+        )
+    }
+}
+
+export default Button
